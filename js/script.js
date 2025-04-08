b9fba6b249cd4ab2c032ffbe884121bc17584823
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-04-08 13:30:00 +0700

    FEAT Desktop design for main & header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const navbar = document.querySelector(".navbar")
 const openNavbarButton = document.querySelector(".open-navbar-button");
 const closeNavbarButton = document.querySelector(".close-navbar-button");
-const media = window.matchMedia("(width < 750px)")
+const media = window.matchMedia("(width < 750px)");
+const headerLogo = document.getElementById("header__logo");
 
 function openNavbar() {
     navbar.classList.add("navbar--show");
@@ -9,7 +10,10 @@ function openNavbar() {
     openNavbarButton.style.display = "none";
     navbar.removeAttribute('inert');
     closeNavbarButton.style.display = "block";
-}
+    headerLogo.style.position = "fixed";
+    headerLogo.style.top = "5vh";
+    headerLogo.style.left = "5vw";
+ }
 
 function closeNavbar() {
     navbar.classList.remove("navbar--show");
@@ -17,6 +21,7 @@ function closeNavbar() {
     navbar.setAttribute('inert','');
     closeNavbarButton.style.display = "none";
     openNavbarButton.style.display = "block";
+    headerLogo.style.position = "static";
 }
 
 function updateNavbar(e) {
